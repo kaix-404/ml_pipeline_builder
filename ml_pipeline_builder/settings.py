62389b83c26a89c29116800b7df3e04b38419651
kaix-404 <kairavnandi@gmail.com>
2026-03-20 22:57:57 +0530

    Updated Debug

--- ml_pipeline_builder/settings.py
+++ ml_pipeline_builder/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3u-+*hd8tp85wmqh!b=q&)+bl=3#=(q3r3+pc2tmnl3*jj8#8g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
